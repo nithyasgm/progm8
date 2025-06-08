c6bba4e9b08a278a7f342b0eb777aea464925976
nithyasgn <nshree80562@gmail.com>
2025-06-08 20:09:53 +0530

    first commit

---
+++ progm8/src/main/java/com/nithyasgm/App.java
@@ -0,0 +1,13 @@
+package com.nithyasgm;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
